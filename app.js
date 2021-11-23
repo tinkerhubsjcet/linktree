8e9ba9e4d70ad18b822faa06e45235ba663a2451
Reenphy George <79626816+reenphygeorge@users.noreply.github.com>
2021-11-24 00:52:56 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+function insta() {
+    window.open("https://www.instagram.com/tinkerhubsjcet", "_blank");
+}
+
+function discord() {
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSf_JiePlR9mFDosSs3eUJlhll-t_wNl3gqSP1ZNjqUrwBAymA/viewform", "_blank");
+}
+
+function linkedin() {
+    window.open("https://www.linkedin.com/company/tinkerhubsjcet", "_blank");
+}
+
+function twitter() {
+    window.open("https://www.twitter.com/tinkerhubsjcet", "_blank");
+}
+
+// Random no generator //
+function randomgenerator(min , max) {
+    let no = Math.random() * max-min +1;
+    no = Math.floor(no) + min;
+    console.log(no);
+    if(no == 1)
+        document.body.style.background = "#215B67";
+    else if(no == 2)
+        document.body.style.background = "#96BF18";
+    else if(no == 3)
+        document.body.style.background = "#EE1702";
+    else if(no == 4)
+        document.body.style.background = "#FECD11";
+    else if(no == 5)
+        document.body.style.background = "#05BFCE";
+    else
+        document.body.style.background = "#0060FF";
+}
