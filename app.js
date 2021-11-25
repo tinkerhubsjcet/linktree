99724d09b2e7aa9e8780d03baa379159de9775ba
Reenphy George <reenphygeorge@gmail.com>
2021-11-25 22:10:51 +0530

    Color aakitond

--- app.js
+++ app.js
@@ -15,20 +15,20 @@ function twitter() {
 }
 
 // Random no generator //
-function randomgenerator(min , max) {
-    let no = Math.random() * max-min +1;
-    no = Math.floor(no) + min;
-    console.log(no);
-    if(no == 1)
-        document.body.style.background = "#215B67";
-    else if(no == 2)
-        document.body.style.background = "#96BF18";
-    else if(no == 3)
-        document.body.style.background = "#EE1702";
-    else if(no == 4)
-        document.body.style.background = "#FECD11";
-    else if(no == 5)
-        document.body.style.background = "#05BFCE";
-    else
-        document.body.style.background = "#0060FF";
-}
+// function randomgenerator(min , max) {
+//     let no = Math.random() * max-min +1;
+//     no = Math.floor(no) + min;
+//     console.log(no);
+//     if(no == 1)
+//         document.body.style.background = "#215B67";
+//     else if(no == 2)
+//         document.body.style.background = "#96BF18";
+//     else if(no == 3)
+//         document.body.style.background = "#EE1702";
+//     else if(no == 4)
+//         document.body.style.background = "#FECD11";
+//     else if(no == 5)
+//         document.body.style.background = "#05BFCE";
+//     else
+//         document.body.style.background = "#0060FF";
+// }
